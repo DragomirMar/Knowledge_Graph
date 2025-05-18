b17976373c32a4b3fe8d7fe64e68b50697f29901
Dragomir Marinov <a11824886@univie.ac.at>
2025-05-18 18:52:29 +0200

    ADDED: class for vector database with first functions

--- vector_database.py
+++ vector_database.py
@@ -1,1 +1,21 @@
-# ToDo: A database class for saving the triples
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+from uuid import uuid4
+
+embeddings = OllamaEmbeddings(
+    model="nomic-embed-text",
+)
+
+class VectorDatabase:
+    
+    def __init__(self):   
+        self.vector_store = Chroma(
+            collection_name="my_collection",
+            embedding_function=embeddings,   
+        )
+    
+    def add_document_triplets(self, triples, source):
+        documents = [Document(page_content=triplet, metadata={"source":source}, id=idx) for idx, triplet in enumerate(triples)]
+        uuids = [str(uuid4()) for _ in range(len(documents))]
+        self.vector_store.add_documents(documents=documents, ids=uuids)
