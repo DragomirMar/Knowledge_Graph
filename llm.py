1f68c41e1ef1e9ad4aef68d5e59c8a62f64954df
Dragomir Marinov <a11824886@univie.ac.at>
2025-06-01 21:30:50 +0200

    ADDED: cleaning commits

--- llm.py
+++
@@ -1,8 +0,0 @@
-from llama_index.llms.ollama import Ollama
-
-class OllamaModel:
-    def __init__(self):
-        self.llm = Ollama(model="llama3.1:8B", request_timeout=120.0, temperature=0.7)
-    
-    def prompt(self, prompt_text):
-        return self.llm.complete(prompt_text).text
